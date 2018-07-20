461dafa858aca60e77a7cf8d65ab517ee367cfd2
Matt Phillips <mattman@mattcave.com>
2018-07-20 13:10:18 -0700

    Simplify for primary use case of copying bytes
    No need for separate go routine and channel for the simple
    case of just moving bytes.

--- main.go
+++ main.go
@@ -11,62 +11,59 @@ import (
 	"time"
 )
 
+const defaultDelay = 10 * time.Millisecond
+
 func main() {
 	var (
 		minDelay time.Duration
 		maxDelay time.Duration
 	)
-	flag.DurationVar(&minDelay, "min", 1*time.Millisecond, "Minium delay between item output")
-	flag.DurationVar(&maxDelay, "max", 10*time.Millisecond, "Maximum delay between item output")
+	flag.DurationVar(&minDelay, "d", defaultDelay, "Uniform delay between item output")
+	flag.DurationVar(&minDelay, "min", defaultDelay, "Minimum variable delaybetween item output")
+	flag.DurationVar(&maxDelay, "max", 0, "Maximum variable delay between item output (defaults to 'min')")
 	flag.Parse()
 
-	data := make(chan []byte)
+	if maxDelay < minDelay {
+		maxDelay = minDelay
+	}
 
-	go func() {
-		defer close(data)
+	delayFunc := newDelayFunc(minDelay, maxDelay)
 
-		if len(flag.Args()) < 1 {
-			readRunes(os.Stdin, data)
-			return
+	if len(flag.Args()) < 1 {
+		copySlow(os.Stdout, os.Stdin, delayFunc)
+		return
+	}
+
+	for _, fname := range flag.Args() {
+		f, err := os.Open(fname)
+		if err != nil {
+			fatal("Error opening %s : %v", fname, err)
 		}
 
-		for _, fname := range flag.Args() {
-			f, err := os.Open(fname)
-			if err != nil {
-				fatal("Error opening %s : %v", fname, err)
+		err = copySlow(os.Stdout, f, delayFunc)
+		f.Close()
+		if err != nil {
+			fatal("Error writing %v", err)
+		}
+	}
+}
+
+func copySlow(w io.Writer, r io.Reader, delayFunc func()) error {
+	buf := bufio.NewReader(r)
+	bs := make([]byte, 1)
+
+	for {
+		b, err := buf.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				break
 			}
-			readRunes(f, data)
-			f.Close()
+			return err
 		}
-	}()
 
-	delay := newDelayFunc(minDelay, maxDelay)
-	err := writeSlow(os.Stdout, data, delay)
-	if err != nil {
-		fatal("Error writing %v", err)
-	}
-}
-
-func fatal(msg string, args ...interface{}) {
-	if !strings.HasSuffix(msg, "\n") {
-		msg = msg + "\n"
-	}
-	fmt.Fprintf(os.Stderr, msg, args)
-}
-
-func readRunes(r io.Reader, data chan<- []byte) {
-	scan := bufio.NewScanner(r)
-	scan.Split(bufio.ScanRunes)
-	for scan.Scan() {
-		r := scan.Text()
-		data <- []byte(r)
-	}
-}
-
-func writeSlow(w io.Writer, data chan []byte, delayFunc func()) error {
-	for b := range data {
 		delayFunc()
-		_, err := w.Write(b)
+		bs[0] = b
+		_, err = w.Write(bs)
 		if err != nil {
 			return err
 		}
@@ -88,4 +85,12 @@ func newDelayFunc(min time.Duration, max time.Duration) func() {
 	return func() {
 		time.Sleep(time.Duration(rand.Intn(shifted) + int(min)))
 	}
+}
+
+func fatal(msg string, args ...interface{}) {
+	if !strings.HasSuffix(msg, "\n") {
+		msg = msg + "\n"
+	}
+	fmt.Fprintf(os.Stderr, msg, args...)
+	os.Exit(1)
 }
